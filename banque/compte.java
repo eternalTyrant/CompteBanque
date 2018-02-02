f28fd23f4c3acebc73ca1460b773923204d6e908
max p <maxence@zendrive.fr>
2018-02-02 11:50:30 +0100

    my_account

---
+++ banque/compte.java
@@ -0,0 +1,47 @@
+package banque;
+
+public class Compte {
+
+    private double solde=0.0;
+
+    public double getSolde(){return solde;}
+    public void setSolde(double solde){this.solde+=solde;}
+
+    Compte(){}
+    Compte(double solde){
+        if(solde<0) solde*=-1;
+        this.solde=solde;
+    }
+
+    public void deposer(double somme){
+        if(somme<0) somme*=-1;
+        this.solde=somme;
+    }
+
+    public double retirer(double somme){
+        double tmp=0;
+        if(solde>=somme) tmp = solde -= somme;
+
+        return tmp;
+    }
+
+    public double virer(double somme, Compte compteDestination){
+
+        if(solde>=somme){
+            compteDestination.setSolde(somme);
+            retirer(somme);
+        }
+        return solde;
+    }
+
+
+    public int main(){
+        toString();
+        Compte my_account = new Compte();
+        Compte good_Account = new Compte(5000.00);
+
+        good_Account.deposer(1000);
+        good_Account.virer(5000.0,my_account);
+        return 0;
+    }
+}
